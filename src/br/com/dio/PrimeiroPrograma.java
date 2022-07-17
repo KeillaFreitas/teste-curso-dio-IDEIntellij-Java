2517bb370b868f12f778b96fe3aa6b2672afb5ef
KeillaFreitas <keillasilvacontabil@gmail.com>
2022-07-17 15:34:37 -0300

    Criando Class Livo

--- src/br/com/dio/PrimeiroPrograma.java
+++ src/br/com/dio/PrimeiroPrograma.java
@@ -7,10 +7,50 @@ public class PrimeiroPrograma {
         Gato gato = new Gato();
         System.out.println(gato);
 
+        Livro livro1 = new Livro("Codigo Limpo", 200 );
+        System.out.println(livro1);
+
+
+
         /* int a = 5;
         int b = 3;
         System.out.println("Hello World!" + (a+b));*/
 
     }
+
 }
 
+class Livro {
+    private String nome;
+    private Integer numpag;
+
+    public Livro(String nome, Integer numpag) {
+        this.nome = nome;
+        this.numpag = numpag;
+
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public Integer getNumpag() {
+        return numpag;
+    }
+
+    public void setNumpag(Integer numpag) {
+        this.numpag = numpag;
+    }
+
+    @Override
+    public String toString() {
+        return "Livro{" +
+                "nome='" + nome + '\'' +
+                ", numpag=" + numpag +
+                '}';
+    }
+}
